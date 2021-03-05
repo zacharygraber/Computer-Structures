ac09d458c87b1e0bd04516ab96bd2b38d7305736
Zachary E. Graber <zegraber@iu.edu>
2021-03-05 15:53:31 -0500

    finished fork.c

---
+++ LAB2/fork.c
@@ -0,0 +1,33 @@
+/***********************************************************************
+ * Filename: fork.c						       *
+ * Description: Implements Spring 2021 CSCI-H 335 Lab 2 part 1	       *
+ * Author: Zachary E. Graber (zegraber@iu.edu)			       *
+ * Date: 3/5/2021						       *
+ ***********************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int input;
+
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		printf("ERROR: Expected 1 positional argument");
+		exit(EXIT_FAILURE);
+	}
+
+	input = atoi(argv[1]);
+	int pid = fork();
+	printf("Hello World\n");
+	
+	// Child
+	if (pid == 0) {
+		printf("%d\n", --input);
+	}
+	// Parent
+	else {
+		printf("%d\n", ++input);
+	}
+	exit(EXIT_SUCCESS);
+}
