d91bb1199d29b6303d9beecaf05fa884cd2f471f
Zachary E. Graber <zegraber@iu.edu>
2021-03-19 17:22:30 -0400

    Completed functionality for part2. Still need to add time testing.

--- LAB3/multiThreads.c
+++ LAB3/multiThreads.c
@@ -5,20 +5,48 @@
  * Date: 3/19/2021                                       *
  *********************************************************/
 
+#define _POSIX_C_SOURCE 2000809L
 #include <stdio.h>
 #include <pthread.h>
 #include <string.h>
 #include <stdlib.h>
 
+typedef struct input_args {
+	char* fname;
+	char* search_string;
+} IA;
 
+void *pthread_routine(void* arg) {
+	IA *args_p = (IA*) arg; // Arg contains the address of an IA struct, so cast it to a struct pointer
+	char* fname = args_p->fname;
+	char* search_string = args_p->search_string;
 
-void *pthread1_routine(void* arg) {
-	
-	return NULL;
-}
+	// Open the file
+	FILE* fp;
+	if ((fp = fopen(fname, "r")) == NULL) {
+		printf("ERROR: could not open file %s. Aborting thread.\n", fname);
+		return NULL;
+	}
 
-void *pthread2_routine(void* arg) {
-	
+	// Read it
+	size_t length = 0;
+	char* thisLine = NULL;
+	char* temp;
+	int targetLen = strlen(search_string);
+	int matches = 0;
+	while (getline(&thisLine, &length, fp) != -1) {
+		temp = thisLine;
+		// Find the first occurrence (if any) of the search string in thisLine and save it back in thisLine
+		while ((temp = strstr(temp, search_string)) != NULL) {
+			matches++;
+		 	// Increment the pointer so it doesn't "find" the same instance again and get stuck.
+		 	temp += targetLen;
+		}
+	}
+
+	// Close the file and finish up
+	fclose(fp);
+	printf("%s Count: %d\n", search_string, matches);
 	return NULL;
 }
 
@@ -28,12 +56,12 @@ int main (int argc, char* argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
-	char* arg1 = argv[1];
-	int arg2 = atoi(argv[2]);
+	IA arg1 = {argv[1], argv[2]};
+	IA arg2 = {argv[3], argv[4]};
 	
 	pthread_t tids[2];
-	pthread_create(&tids[0], NULL, pthread1_routine, (void*) &arg1);
-	pthread_create(&tids[1], NULL, pthread2_routine, (void*) &arg2);
+	pthread_create(&tids[0], NULL, pthread_routine, (void*) &arg1);
+	pthread_create(&tids[1], NULL, pthread_routine, (void*) &arg2);
 
 	pthread_join(tids[0], NULL);
 	pthread_join(tids[1], NULL);
