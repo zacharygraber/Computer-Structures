84434621566658628b7e131a227b0eec369bd170
Zachary E Graber <zegraber@iu.edu>
2021-03-26 16:21:28 -0400

    aahhhhhh

--- LAB4/prodcons_mutex.c
+++ LAB4/prodcons_mutex.c
@@ -28,7 +28,7 @@ void *pthread_routine(void* arg) {
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
+    if (argc < 2) {
         printf("ERROR: Expected 1 positional argument <n>\n");
         exit(EXIT_FAILURE);
     }
