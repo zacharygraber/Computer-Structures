05eb8bdb9c73cfc7815a78dbb086bb2bea4b626c
Zachary E. Graber <zegraber@iu.edu>
2021-05-05 23:01:39 -0400

    fine, I'll do it myself

--- LAB8/float2bitstr.c
+++ LAB8/float2bitstr.c
@@ -6,8 +6,8 @@
  **************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
+#include <math.h>
 
 #define BIT_DEPTH 32
 #define EXP_SIZE 8
@@ -17,6 +17,7 @@
 void toBinaryString(int num, int numbits, char* target);
 void toBinaryStringUnsigned(int num, int numbits, char* target);
 void toBinaryStringFractional(double num, int numbits, char* target);
+double pow2Negative(int exponent);
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -147,7 +148,7 @@ void toBinaryStringFractional(double num, int numbits, char* target) {
     int i;
     // Go from left to right, starting at 1/2, then 1/4, then 1/8 and so on
     for (i = 0; i < numbits; i++) {
-        double power_of_two = pow(2, -1* (i+1));
+        double power_of_two = pow2Negative((i+1));
         if (num >= power_of_two) {
             result[i] = '1';
             num -= power_of_two;
@@ -156,4 +157,9 @@ void toBinaryStringFractional(double num, int numbits, char* target) {
             result[i] = '0';
         }
     }
+}
+
+double pow2Negative(int exponent) {
+	double answers[] = {1.0, 0.5, 0.25, 0.125, 0.0625, 0.03125, 0.015625, 0.0078125, 0.00390625, 0.001953125, 0.0009765625, 0.00048828125, 0.000244140625, 0.0001220703125, 6.103515625e-05, 3.0517578125e-05, 1.52587890625e-05, 7.62939453125e-06, 3.814697265625e-06, 1.9073486328125e-06, 9.5367431640625e-07, 4.76837158203125e-07, 2.384185791015625e-07, 1.1920928955078125e-07, 5.960464477539063e-08, 2.9802322387695312e-08, 1.4901161193847656e-08, 7.450580596923828e-09, 3.725290298461914e-09, 1.862645149230957e-09, 9.313225746154785e-10, 4.656612873077393e-10, 2.3283064365386963e-10, 1.1641532182693481e-10, 5.820766091346741e-11, 2.9103830456733704e-11, 1.4551915228366852e-11, 7.275957614183426e-12, 3.637978807091713e-12, 1.8189894035458565e-12, 9.094947017729282e-13, 4.547473508864641e-13, 2.2737367544323206e-13, 1.1368683772161603e-13, 5.684341886080802e-14, 2.842170943040401e-14, 1.4210854715202004e-14, 7.105427357601002e-15, 3.552713678800501e-15, 1.7763568394002505e-15, 8.881784197001252e-16, 4.440892098500626e-16, 2.220446049250313e-16, 1.1102230246251565e-16, 5.551115123125783e-17, 2.7755575615628914e-17, 1.3877787807814457e-17, 6.938893903907228e-18, 3.469446951953614e-18, 1.734723475976807e-18, 8.673617379884035e-19, 4.336808689942018e-19, 2.168404344971009e-19, 1.0842021724855044e-19};
+	return answers[exponent];
 }
