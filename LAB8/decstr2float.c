04cd23d88453223cb7f9468f293b5e8637168f04
Zachary E. Graber <zegraber@iu.edu>
2021-05-05 23:56:42 -0400

    ahhhh

--- LAB8/decstr2float.c
+++ LAB8/decstr2float.c
@@ -13,7 +13,16 @@
 #define MANTISSA_SIZE 23
 #define BIAS 127
 
-int binary_to_int(char* bin_num);
+typedef union {
+    double result;
+    struct {
+        unsigned long mantissa : MANTISSA_SIZE;
+        unsigned long exp : EXP_SIZE;
+        int sign : 1;
+    } parts;
+} union_special_thing;
+
+unsigned long binary_to_int(char* bin_num, int numbits);
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -21,10 +30,6 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
     char* input = argv[1];
-    if (strlen(input) != BIT_DEPTH) {
-        printf("Incorrect input argument\n");
-        exit(EXIT_FAILURE);
-    }
     int i;
     for (i = 0; i < BIT_DEPTH; i++) {
         if (input[i] != '1' || input[i] != '0') {
@@ -33,16 +38,32 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    union_special_thing input_struct;
     printf("Sign: %c\n", input[0]);
+    input_struct.parts.sign = input[0] == '1' ? 1 : 0;
+
+    char exp[EXP_SIZE];
+    strncpy(exp, input+1, EXP_SIZE);
+    int e = binary_to_int(exp, BIT_DEPTH);
+    input_struct.parts.exp = e;
+    printf("Exponent: %u\n", e);
+
+    char mantissa_string[MANTISSA_SIZE];
+    strncpy(mantissa_string, input+1+EXP_SIZE, MANTISSA_SIZE);
+    unsigned long mantissa = binary_to_int(mantissa_string, MANTISSA_SIZE);
+    printf("Mantissa: %.17g\n", mantissa);
+    input_struct.parts.mantissa = mantissa;
+
+    printf("Number: %.7g\n", input_struct.result);
 
     exit(EXIT_SUCCESS);
 }
 
-int binary_to_int(char* bin_num) {
-	int result = 0;
+unsigned long binary_to_int(char* bin_num, int numbits) {
+	unsigned long result = 0;
 	int i;
-	for (i = 0; i < BIT_DEPTH; i++) { // i is the index of the bit, with 0 being the MSB and BIT_DEPTH-1 the LSB
-		unsigned long power_of_two = BIT_DEPTH - (i + 1); // The power of two scalar associated with this bit index
+	for (i = 0; i < numbits; i++) { // i is the index of the bit, with 0 being the MSB and BIT_DEPTH-1 the LSB
+		unsigned long power_of_two = numbits - (i + 1); // The power of two scalar associated with this bit index
 		if (bin_num[i] == '1') {
 			result += (1 << power_of_two); // Left-shifting 1 by power_of_two is == to multiplying 1 by 2^(power_of_two)
 		}
